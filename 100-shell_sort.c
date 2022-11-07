acb8fa026957f7de1079f482c542ae7abd475763
“sadewale72” <adewaleaderoju202@yahoo.com>
2022-11-07 00:05:08 -0800

    My fifth commit

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,30 +8,30 @@
  */
 void shell_sort(int *array, size_t size)
 {
-unsigned int i = 0, j = 0, gap = 0;
-int aux = 0;
+	unsigned int i = 0, j = 0, gap = 0;
+	int aux = 0;
 
-if (array == NULL || size < 2)
-return;
+	if (array == NULL || size < 2)
+		return;
 
-while (gap < size / 3)
-gap = gap * 3 + 1;
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
 
-for (; gap > 0; gap = (gap - 1) / 3)
-{
-for (i = gap; i < size; i++)
-{
-aux = array[i];
-for (j = i; j >= gap && array[j - gap] > aux;
-j -= gap)
-{
-if (array[j] != array[j - gap])
-array[j] = array[j - gap];
-}
-if (array[j] != aux)
-array[j] = aux;
+	for (; gap > 0; gap = (gap - 1) / 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			aux = array[i];
+			for (j = i; j >= gap && array[j - gap] > aux;
+					j -= gap)
+			{
+				if (array[j] != array[j - gap])
+					array[j] = array[j - gap];
+			}
+			if (array[j] != aux)
+				array[j] = aux;
 
-}
-print_array(array, size);
-}
+		}
+		print_array(array, size);
+	}
 }
